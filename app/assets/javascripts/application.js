1f68b8db54d19cbf2fea2706a36fec0a3612ef1a
Maciorex <maciej.zabski@gmail.com>
2018-06-14 16:59:59 +0200

    Add image validation, add ingredients and directions models

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require activestorage
 //= require jquery
 //= require bootstrap-sprockets
+//= require cocoon
 //= require turbolinks
 //= require_tree .
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,24 @@
 class Recipe < ApplicationRecord
-  has_one_attached :image
   validates :title, uniqueness: true
   validates :description, presence: true
+  validate :correct_image
+  has_one_attached :image
+  has_many :ingredients
+  has_many :directions
+
+  accepts_nested_attributes_for :ingredients,
+                                reject_if: :all_blank,
+                                allow_destroy: true
+  accepts_nested_attributes_for :directions,
+                                reject_if: :all_blank,
+                                allow_destroy: true
+
+  private
+  def correct_image
+    if image.attached? && !image.content_type.in?(%w( image/img image/jpg image/jpeg image/png))
+      errors.add(:image, "Incorrect format type")
+    elsif !image.attached?
+      errors.add(:image, "Image is missing")
+    end  
+  end
 end
